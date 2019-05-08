be1abcf04553092b7cf5732837d2dafdf7e267a6
Ngichu <brianngichu@gmail.com>
2019-05-08 15:06:42 +0300

    added jquery toggle method

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".Design").click(function(){
+      $(".design-showing").toggle();
+      $(".design-hidden").toggle();
+    });
+    $(".Development").click(function() {
+      $(".development-showing").toggle();
+      $(".development-hidden").toggle();
+    });
+    $(".Product").click(function() {
+      $(".product-showing").toggle();
+      $(".product-hidden").toggle();
+    });
