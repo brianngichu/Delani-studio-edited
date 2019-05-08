6cc7c69e9aec2ef271bc0d69b6d821b4cf6113a7
Ngichu <brianngichu@gmail.com>
2019-05-08 15:10:12 +0300

    added function to alert user

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,36 @@ $(document).ready(function() {
       $(".product-showing").toggle();
       $(".product-hidden").toggle();
     });
+    $("#submit").click(function(event){
+        var name = $("#name").val();
+        alert(name + " Thanks. We have received your message.");
+        event.preventDefault();
+        var name = $("#name").val()
+        var email = $("#email").val()
+        var message = $("#message").val()
+        var status = $("#submit")
+        statusElm.empty()
+  
+        if(email.length > 5 && email.includes("@") && email.includes(".")) {
+        statusElm.append("<div>Email is valid</div>")
+        }
+        else {
+        statusElm.append("<div>Email is not valid</div>")
+        }
+  
+        if (name.length > 1) {
+        statusElm.append("<div>Name is valid</div>")
+        }
+        else {
+        statusElm.append("<div>Name is not valid valid</div>")
+        }
+  
+        if (message.length > 8) {
+        statusElm.append("<div>message is valid</div>")
+        }
+        else {
+        statusElm.append("<div>message is not valid valid</div>")
+        }
+    })
+  });   
+  
